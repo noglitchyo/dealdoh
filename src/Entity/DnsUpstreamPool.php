af616c6cfcdf09237c8077c990d7e22dd3f5e4ab
Maxime ELOMARI <764791+noglitchyo@users.noreply.github.com>
2019-06-14 14:05:18 +0200

    Added command to add upstream to upstreampool

--- src/Entity/DnsUpstreamPool.php
+++ src/Entity/DnsUpstreamPool.php
@@ -2,32 +2,53 @@
 
 namespace NoGlitchYo\Dealdoh\Entity;
 
+use JsonSerializable;
+
 /**
  * @codeCoverageIgnore
  */
-class DnsUpstreamPool
+class DnsUpstreamPool implements JsonSerializable
 {
     /**
      * @var array
      */
     private $dnsUpstreams = [];
 
+    public static function fromJson(string $jsonUpstreamPool): self
+    {
+        $upstreamPool = json_decode($jsonUpstreamPool, true);
+
+        return new static($upstreamPool);
+    }
+
     public function __construct(array $dnsUpstreams = [])
     {
         foreach ($dnsUpstreams as $dnsUpstream) {
-            $this->addUpstream(new DnsUpstream($dnsUpstream));
+            // TODO: cover this with test case
+            if (!is_array($dnsUpstream)) {
+                $dnsUpstream['uri'] = $dnsUpstream;
+            }
+
+            $this->addUpstream(new DnsUpstream($dnsUpstream['uri'], $dnsUpstream['code'] ?? null));
         }
     }
 
-    public function addUpstream(DnsUpstream $dnsUpstream): void
+    public function addUpstream(DnsUpstream $dnsUpstream): self
     {
         $this->dnsUpstreams[] = $dnsUpstream;
+
+        return $this;
     }
 
     /**
      * @return DnsUpstream[]
      */
     public function getUpstreams(): array
+    {
+        return $this->dnsUpstreams;
+    }
+
+    public function jsonSerialize(): array
     {
         return $this->dnsUpstreams;
     }
